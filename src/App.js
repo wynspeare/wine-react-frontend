5080519b311234f7345d88c074ce80a5a7a31017
Wynspeare <wynspeare@gmail.com>
2019-03-15 14:09:35 -0500

    AddWinePage setup

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage.js';
 import WinePage from './pages/WinePage.js';
+import AddWinePage from './pages/AddWinePage.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <BrowserRouter>
           <div>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/add-wine" component={AddWinePage} />
             <Route exact path="/wines/:wineID" component={WinePage} />
           </div>
         </BrowserRouter>
--- src/pages/WinePage.js
+++ src/pages/WinePage.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import WineAPI from '../api/WineAPI.js'
 
 class WinePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wine: {}
+    };
+  }
+
+  componentDidMount() {
+    let id = this.props.match.params.wineID;
+    WineAPI.fetchWineByID(id)
+      .then((wine) => this.setState({
+        wine: wine
+    }));
+  }
+
   render() {
     return (
-      <div> 
-        <h1> Wine Page </h1>
+      <div>
+        <h2> Name </h2>
+        <p> {this.state.wine['name']}</p>
+        <h2> Price </h2>
+        <p> {this.state.wine['price']}</p>
+        <h2> Varietal </h2>
+        <p> {this.state.wine['varietal']}</p>
+        <h2> Description </h2>
+        <p> {this.state.wine['description']}</p>
       </div>
     );
   }
