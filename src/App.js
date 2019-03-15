af7dd306754bad4be00796f83efb2eb6dabf865b
Wynspeare <wynspeare@gmail.com>
2019-03-15 13:24:24 -0500

    HomePage and WinePage built

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage.js';
+import WinePage from './pages/WinePage.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/wines/:wineID" component={WinePage} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/pages/WinePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WinePage extends Component {
+  render() {
+    return (
+      <div> 
+        <h1> Wine Page </h1>
+      </div>
+    );
+  }
+}
+
+export default WinePage;
